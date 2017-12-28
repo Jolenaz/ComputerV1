67af23ec2bae77be15217fa067b31b37e4d7929c
Jonas BELLESSA <jbelless@e3r7p21.42.fr>
2017-12-28 17:24:29 +0100

    bon debut

---
+++ computor.py
@@ -0,0 +1,11 @@
+
+import sys
+from parseInput import parseInput
+
+coeff = parseInput(sys.argv)
+
+if (coeff == 10):
+    quit()
+
+print("ok bro")
+
---
+++ parseInput.py
@@ -0,0 +1,8 @@
+
+def parseInput(input):
+    inputStr = ""
+    for i in range(1,len(input)):
+        inputStr += input[i]
+    inputStr = inputStr.replace(" ", "")
+    print(inputStr)
+    return (10)
